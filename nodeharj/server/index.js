9fbe63e0e19d99c457af70d01f2a3be2384829be
siirtola1 <aleksi.siirtola9@gmail.com>
2022-10-17 14:31:12 +0300

    ready

---
+++ nodeharj/server/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cors = require('cors')
+const mysql=require('mysql2/promise')
+const config=require('./config')
+
+
+const app =express()
+app.use(cors())
+
+const port = 3001
+
+app.get("/",async (req, res) =>{
+    try{
+        const connection = await mysql.createConnection(config.db)
+        const [result,fields] = await connection.execute('select * from task')
+        if (!result) result=[]
+
+        res.status(200).json(result)
+    } catch(err){
+        res.status(500).json({error:err.message})
+    }
+})
+
+app.listen(port)
